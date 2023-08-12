c0b4d4957d8ea960a9174c568e495292a85e9756
GeorginaCarlisle <>
2023-08-12 11:07:46 +0000

    Remove un-needed update notebook from check_location_type

--- run.py
+++ run.py
@@ -604,7 +604,6 @@ def check_location_type(location_number, current_case):
     """
     locations = SHEET.worksheet("locations")
     location_name = locations.cell(location_number, 1).value
-    update_notebook(current_case.notebook_column, [f"You visted {location_name}"])
     if location_name == current_case.stash_location:
         visit_stash_location(current_case)
     elif location_name == current_case.pre_crime_location:
